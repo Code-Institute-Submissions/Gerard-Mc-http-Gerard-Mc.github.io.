8d843b9b8e54e1b427021c7eed33abb1ee2fbd70
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-01-18 19:55:50 +0000

    New background + js class contructor

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+class ApeBrain {
+	constructor() {
+		this.startButtonEl = document.getElementById("start-button");
+		this.resetButtonEl = document.getElementById("reset-button");
+		this.score = 0;
+		this.boxes = 9;
+		this.startGameCountDownTime = 3;
+	}
+    
+const apeBrain = new ApeBrain();
